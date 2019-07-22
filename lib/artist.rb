14b83e3ebcf0d959a792d6bcfd5337c0b808d598
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-22 03:12:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,6 +6,13 @@ class Artist
   @name = name 
   @@songs = []
   
-   
+  def add_song(song)
+     @@songs << song
+     song.artist = self
+   end
+  
+   def artist_name
+    self.artist.name
+  end
   
 end

1b96516454a88b644bbc0f58792336f56211a330
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-22 04:52:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,31 +1,18 @@
 class Song
-  attr_accessor :artist, :name, :artist_name
-    @@all = []
-    
-    
+  attr_accessor :name, :artist
+
+  @@all = []
+
   def initialize(name)
-  @name = name
-  @artist = artist #this might not be correct
-  @@all << self
+    @name = name
+    @@all << self
   end
 
-  def add_song(song)
-     @@all << song
-     song.artist = self
-   end
-   
-   def self.all
-     @@all 
-   end
-   
+  def self.all
+    @@all
+  end
+
   def artist_name
     artist.name if artist
   end
-end
- 
- 
- 
- 
- 
- 
 end

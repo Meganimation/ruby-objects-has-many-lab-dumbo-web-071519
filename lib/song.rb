00945f0f528f16cf48a5d57ac79180f457edad68
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-22 04:51:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,14 +18,10 @@ class Song
      @@all 
    end
    
-   #below one isnt correct answer
-   def self.find_by_name(artist_name)
- if   @@all.find{|person| person.name == artist_name}
-  artist_name 
-else  nil
+  def artist_name
+    artist.name if artist
   end
 end
- 
  
  
  

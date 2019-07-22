730c577e75f05fea95fd5b39b4e1536ddf8f4ce0
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-22 03:11:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,9 @@
 class Artist 
   
- 
+  attr_accessor :name 
   
-  initialize(songs)
-
+  initialize(name)
+  @name = name 
   @@songs = []
   
   def add_song(song)
@@ -11,8 +11,6 @@ class Artist
      song.artist = self
    end
   
-   def artist_name
-    self.artist.name
-  end
+   
   
 end

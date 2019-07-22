a2391a6c8f8e4227f83b39d41f93d09a7a3c7719
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-22 04:17:47 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,27 +1,4 @@
 class Post 
   
-  attr_accessor :title
   
-
-  
-  def initialize(title)
-  @title = title 
-
-end
-
-def posts
-   Posts.all.select do |post|
-    post.author == self
-end
-  
-
-def self.all 
-  @@all
-end
-
-
-
-def all
-  @@all
-end
-end
+end 
